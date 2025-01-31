169b9a7a7a8e0b30db0b9fe0310c6a6840d92f29
Moditha Marasingha <marasinghamoditha51@gmail.com>
2025-01-31 21:01:35 +0530

    education section motion added

--- components/education.tsx
+++ components/education.tsx
@@ -1,8 +1,9 @@
 "use client";
 
 import React from "react";
+import { useRef } from "react";
 import SectionHeading from "./sectionHeading";
-import { motion } from "framer-motion";
+import { motion, useScroll, useTransform } from "framer-motion";
 import { EducationCard } from "./ui/education-card";
 
 export default function Education() {
@@ -26,13 +27,23 @@ export default function Education() {
         },
       ];
 
+    const ref = useRef<HTMLDivElement>(null);
+    const { scrollYProgress } = useScroll({
+        target: ref,
+        offset: ["0 1", "1.33 1"],
+      });
+    const scaleProgess = useTransform(scrollYProgress, [0, 1], [0.8, 1]);
+    const opacityProgess = useTransform(scrollYProgress, [0, 1], [0.6, 1]);
+
   return (
     <motion.section
       className="mb-28 max-w-[45rem] text-center sm:mb-40 scroll-mt-28 w-full"
-      initial={{ opacity: 0, y: 100 }}
-      animate={{ opacity: 1, y: 0 }}
-      transition={{ delay: 0.175 }}
       id="education"
+      ref={ref}
+      style={{
+        scale: scaleProgess,
+        opacity: opacityProgess,
+      }}
     >
       <SectionHeading>Education</SectionHeading>
       
