9987089fbb6e5c696de43b9eedbd1859643d98d3
Moditha Marasingha <marasinghamoditha51@gmail.com>
2025-02-09 13:26:03 +0530

    add box shadow to dock

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,9 @@ export default {
         foreground: "var(--foreground)",
       },
     },
+    boxShadow: {
+      lg: '0 0 15px -3px rgba(0, 0, 0, 0.1), 0 4px 6px -2px rgba(0, 0, 0, 0.05)',
+    },
   },
   plugins: [],
 } satisfies Config;
