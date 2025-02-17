207938a9e5ab8f5078ed534247d21e0c5e2c39c7
Moditha Marasingha <marasinghamoditha51@gmail.com>
2025-02-17 18:51:02 +0530

    Metadata Added

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,6 +9,20 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Moditha Marasingha",
   description: "Explore the portfolio of Moditha Marasingha, a passionate Software Engineering undergraduate specializing in backend development, web solutions, and IoT projects. Showcasing expertise in Java, Spring Boot, React, MongoDB, and more.",
+  openGraph: {
+    title: "Moditha Marasingha",
+    description: "Explore the portfolio of Moditha Marasingha, a passionate Software Engineering undergraduate specializing in backend development, web solutions, and IoT projects. Showcasing expertise in Java, Spring Boot, React, MongoDB, and more.",
+    url: "https://www.moditha.me/",
+    type: "website",
+    images: [
+      {
+        url: "/me.jpeg",
+        width: 1200,
+        height: 630,
+        alt: "Portfolio Banner",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
