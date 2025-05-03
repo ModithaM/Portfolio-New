912f34465ecbbf5cd58c519f5eec4f0948af1dc1
Moditha Marasingha <marasinghamoditha51@gmail.com>
2025-05-03 18:26:35 +0530

    Education card spelling mistake fixed

--- components/education.tsx
+++ components/education.tsx
@@ -8,32 +8,32 @@ import { EducationCard } from "./ui/education-card";
 
 export default function Education() {
 
-   const education = [
-        {
-          school: "SLIIT",
-          href: "https://www.sliit.lk/",
-          degree: "Bachelor's Degree of Information Technology (BCS)",
-          logoUrl: "/sliit.png",
-          start: "2023",
-          end: "Present",
-        },
-        {
-          school: "Kegalu Vidyalaya",
-          href: "https://kegaluvidyalaya.lk/",
-          degree: "GCE Advanced Level",
-          logoUrl: "/kv.jpeg",
-          start: "2009",
-          end: "2022",
-        },
-      ];
+  const education = [
+    {
+      school: "SLIIT",
+      href: "https://www.sliit.lk/",
+      degree: "Bachelor's Degree of Information Technology (BSc)",
+      logoUrl: "/sliit.png",
+      start: "2023",
+      end: "Present",
+    },
+    {
+      school: "Kegalu Vidyalaya",
+      href: "https://kegaluvidyalaya.lk/",
+      degree: "GCE Advanced Level",
+      logoUrl: "/kv.jpeg",
+      start: "2009",
+      end: "2022",
+    },
+  ];
 
-    const ref = useRef<HTMLDivElement>(null);
-    const { scrollYProgress } = useScroll({
-        target: ref,
-        offset: ["0 1", "1.33 1"],
-      });
-    const scaleProgess = useTransform(scrollYProgress, [0, 1], [0.8, 1]);
-    const opacityProgess = useTransform(scrollYProgress, [0, 1], [0.6, 1]);
+  const ref = useRef<HTMLDivElement>(null);
+  const { scrollYProgress } = useScroll({
+    target: ref,
+    offset: ["0 1", "1.33 1"],
+  });
+  const scaleProgess = useTransform(scrollYProgress, [0, 1], [0.8, 1]);
+  const opacityProgess = useTransform(scrollYProgress, [0, 1], [0.6, 1]);
 
   return (
     <motion.section
@@ -46,20 +46,20 @@ export default function Education() {
       }}
     >
       <SectionHeading>Education</SectionHeading>
-      
+
       <div className="flex min-h-0 flex-col gap-y-3 ">
-          {education.map((education) => (
-              <EducationCard
-                key={education.school}
-                href={education.href}
-                logoUrl={education.logoUrl}
-                altText={education.school}
-                title={education.school}
-                subtitle={education.degree}
-                period={`${education.start} - ${education.end}`}
-              />
-          ))}
-        </div>
+        {education.map((education) => (
+          <EducationCard
+            key={education.school}
+            href={education.href}
+            logoUrl={education.logoUrl}
+            altText={education.school}
+            title={education.school}
+            subtitle={education.degree}
+            period={`${education.start} - ${education.end}`}
+          />
+        ))}
+      </div>
 
     </motion.section>
   );
