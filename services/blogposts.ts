8de21d5c62018ef122a7ce45b74e5394018cb663
Moditha Marasingha <marasinghamoditha51@gmail.com>
2026-02-10 19:21:15 +0530

    fix: simplify API request parameters in getAllPosts function

--- services/blogposts.ts
+++ services/blogposts.ts
@@ -4,13 +4,8 @@ import {apiClient} from "@/config/apiConfig";
 export const getAllPosts = async (): Promise<BlogApiItem[]> => {
     try {
         const response = await apiClient.get<BlogApiResponse>("https://api.rss2json.com/v1/api.json",
-            {
-                params: {
-                    rss_url: "https://medium.com/feed/@moditham",
-                },
-            }
+            { params: { rss_url: "https://medium.com/feed/@moditham" } }
         );
-
         return response.data.items;
     } catch {
         throw new Error("Failed to fetch blog posts");
