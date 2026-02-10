e2ff2756dda1301ee2d41ee61d880cf3c71551ec
Moditha Marasingha <marasinghamoditha51@gmail.com>
2026-02-10 12:26:47 +0530

    feat: add API client and update blog components to use new data structure

--- components/ui/blog-card.tsx
+++ components/ui/blog-card.tsx
@@ -10,26 +10,15 @@ import Image from "next/image";
 import Link from "next/link";
 import Markdown from "react-markdown";
 import { Icons } from "./icons";
+import {Post} from "@/types";
 
 interface Props {
-  title: string;
-  description: string;
-  dates: string;
-  link?: string;
-  image?: string;
-  video?: string;
-  links?: string ;
+  post: Post;
   className?: string;
 }
 
 export function BlogCard({
-  title,
-  description,
-  dates,
-  link,
-  image,
-  video,
-  links,
+  post,
   className,
 }: Props) {
   return (
@@ -39,12 +28,12 @@ export function BlogCard({
       }
     >
       <Link
-        href={links || "#"}
+        href={post.link || "#"}
         className={cn("block cursor-pointer", className)}
       >
-        {video && (
+        {post.video && (
           <video
-            src={video}
+            src={post.video}
             autoPlay
             loop
             muted
@@ -52,10 +41,10 @@ export function BlogCard({
             className="pointer-events-none mx-auto h-40 w-full object-cover object-top" // needed because random black line at bottom of video
           />
         )}
-        {image && (
+        {post.image && (
           <Image
-            src={image}
-            alt={title}
+            src={post.image}
+            alt={post.title}
             width={500}
             height={300}
             className="h-40 w-full overflow-hidden object-cover object-top"
@@ -64,20 +53,20 @@ export function BlogCard({
       </Link>
       <CardHeader className="px-2">
         <div className="space-y-1 text-left">
-          <CardTitle className="mt-1 text-base">{title}</CardTitle>
-          <time className="font-sans text-xs">{dates}</time>
+          <CardTitle className="mt-1 text-base">{post.title}</CardTitle>
+          <time className="font-sans text-xs">{post.pubDate}</time>
           <div className="hidden font-sans text-xs underline print:visible">
-            {link?.replace("https://", "").replace("www.", "").replace("/", "")}
+            {post.link?.replace("https://", "").replace("www.", "").replace("/", "")}
           </div>
           <Markdown className="prose max-w-full text-pretty font-sans text-xs text-muted-foreground dark:prose-invert">
-            {description}
+            {post.description}
           </Markdown>
         </div>
       </CardHeader>
       <CardFooter className="px-2 pb-2">
-        {links && links.length > 0 && (
+        {post.link && post.link.length > 0 && (
           <div className="flex flex-row flex-wrap items-start gap-1">
-              <Link href={links} target="_blank">
+              <Link href={post.link} target="_blank" rel="noopener noreferrer">
                 <Badge className="flex gap-2 px-2 py-1 text-[10px]">
                   {<Icons.globe className="size-3" />}
                   {"Read More"}
